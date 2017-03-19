8db086fe41a605eb7d1f5d6ea5585cdaa3abf5ac
Chris <cfmcginn@mit.edu>
2017-03-19 17:43:27 +0100

    Modded plugins.cc for use with PuWithNtuple subtractor, which outputs trees with pu vars for testing

--- RecoHI/HiJetAlgos/plugins/plugins.cc
+++ RecoHI/HiJetAlgos/plugins/plugins.cc
@@ -2,6 +2,7 @@
 #include "RecoHI/HiJetAlgos/interface/JetOffsetCorrector.h"
 #include "RecoHI/HiJetAlgos/interface/ParametrizedSubtractor.h"
 #include "RecoHI/HiJetAlgos/interface/ReflectedIterator.h"
+#include "RecoHI/HiJetAlgos/interface/PuWithNtuple.h"
 
 #include "FWCore/PluginManager/interface/ModuleDef.h"
 #include "FWCore/Framework/interface/MakerMacros.h"
@@ -12,6 +13,7 @@ DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,JetOffsetCorrector,"JetOffsetCorrector
 DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,MultipleAlgoIterator,"MultipleAlgoIterator");
 DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,ParametrizedSubtractor,"ParametrizedSubtractor");
 DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,ReflectedIterator,"ReflectedIterator");
+DEFINE_EDM_PLUGIN(PileUpSubtractorFactory,PuWithNtuple,"PuWithNtuple");
 
 
 #include "RecoHI/HiJetAlgos/interface/ParticleTowerProducer.h"
