e3a6e9737f1916bdcc724705e3afc88cbddbf697
Dhanush Hangal <dhanga2@uic.edu>
2018-04-11 21:21:08 +0200

    dataconfig no. of events

--- HeavyIonsAnalysis/JetAnalysis/test/runForestAOD_PbPb_DATA_75X.py
+++ HeavyIonsAnalysis/JetAnalysis/test/runForestAOD_PbPb_DATA_75X.py
@@ -33,7 +33,7 @@ process.source = cms.Source("PoolSource",
 
 # Number of events we want to process, -1 = all events
 process.maxEvents = cms.untracked.PSet(
-    input = cms.untracked.int32(100))
+    input = cms.untracked.int32(1))
 
 process.output = cms.OutputModule("PoolOutputModule",
                                   outputCommands = cms.untracked.vstring('drop *',
